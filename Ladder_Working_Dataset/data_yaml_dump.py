c5f1042e91c372bb37dd4ac08e95e05631781a4a
JooJoo <68879766+zhuyuan7@users.noreply.github.com>
2021-09-23 15:52:54 +0900

    dataset

---
+++ Ladder_Working_Dataset/data_yaml_dump.py
@@ -0,0 +1,23 @@
+#yaml을 이용해 data.yaml의 train set과 valid set의 경로를 바꾸어주도록 하자
+
+import yaml
+ 
+with open('/home/joo/jw_detection/Ladder_Working_Dataset/data.yaml', 'r') as f:
+	data = yaml.load(f)
+    
+#print(data)
+ 
+data['train'] = '/home/joo/jw_detection/Ladder_Working_Dataset/train.txt'
+data['val'] = '/home/joo/jw_detection/Ladder_Working_Dataset/valid.txt'
+data['test'] = '/home/joo/jw_detection/Ladder_Working_Dataset/test.txt'
+
+with open('/home/joo/jw_detection/Ladder_Working_Dataset/data.yaml', 'w') as f:
+	yaml.dump(data, f)
+    
+print(data)
+
+# Print classes
+with open('/home/joo/jw_detection/Ladder_Working_Dataset/data.yaml') as f:
+  d = yaml.load(f, Loader=yaml.FullLoader)  # dict
+  for i, x in enumerate(d['names']):
+    print(i, x)
