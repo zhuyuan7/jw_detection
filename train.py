fdb1a4520bee57e74ba3f651312275ad452d87ae
JooJoo <68879766+zhuyuan7@users.noreply.github.com>
2021-09-29 21:31:10 +0900

    model = torch hub
     model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True) # load torch hub

--- train.py
+++ train.py
@@ -39,7 +39,7 @@ if str(ROOT) not in sys.path:
 
 import val  # for end-of-epoch mAP
 from models.experimental import attempt_load
-from models.yolo import Model
+# from models.yolo import Model
 from utils.autoanchor import check_anchors
 from utils.datasets import create_dataloader
 from utils.general import labels_to_class_weights, increment_path, labels_to_image_weights, init_seeds, \
@@ -119,7 +119,10 @@ def train(hyp,  # path/to/hyp.yaml or hyp dictionary
         with torch_distributed_zero_first(RANK):
             weights = attempt_download(weights)  # download if not found locally
         ckpt = torch.load(weights, map_location=device)  # load checkpoint
-        model = Model(cfg or ckpt['model'].yaml, ch=3, nc=nc, anchors=hyp.get('anchors')).to(device)  # create
+        
+        #model = Model(cfg or ckpt['model'].yaml, ch=3, nc=nc, anchors=hyp.get('anchors')).to(device)  # create
+        model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True) # load torch hub
+        
         exclude = ['anchor'] if (cfg or hyp.get('anchors')) and not resume else []  # exclude keys
         csd = ckpt['model'].float().state_dict()  # checkpoint state_dict as FP32
         csd = intersect_dicts(csd, model.state_dict(), exclude=exclude)  # intersect
